86cb346cfa5da5c8e67b273adcc072ef681126a1
Your Color <noorhost0@gmail.com>
2023-12-17 05:32:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
-const addonInterface = require("./addon")
-serveHTTP(addonInterface, { port: process.env.PORT || 63657 })
+const { serveHTTP, publishToCentral } = require("stremio-addon-sdk");
+const addonInterface = require("./addon");
+serveHTTP(addonInterface, { port: process.env.PORT || 63657 });
 
-// when you've deployed your addon, un-comment this line
-publishToCentral("http://8e55bfcf6ffb-stremionow.baby-beamup.club/manifest.json")
-// for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
+// Uncomment the line below when you've deployed your addon
+publishToCentral("http://8e55bfcf6ffb-stremionow.baby-beamup.club/manifest.json");
+// For more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
